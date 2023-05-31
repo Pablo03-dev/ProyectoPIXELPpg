5a3c20c7578b64c9d8c4c15968afcfccc695bbcd
leananthony <61725039+leananthony@users.noreply.github.com>
2023-05-31 18:39:08 -0400

    uwu2

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
         {
             transform.position += transform.right * speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += transform.up * jump * Time.deltaTime;
             particulaSalto.SetActive(true);
